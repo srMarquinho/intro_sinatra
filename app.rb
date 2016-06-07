ccb7968acbed0346e0dc7401c18dc6b3dc0155ea
srMarquinho <sr.marquinho@gmail.com>
2016-06-07 13:01:05 +0100

    add extra pages to app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 
-get '/:name' do |name|
-  puts "Hello World"
-  name
+get '/' do
+  a = 4 * 7
+  "#{a}"
+end
+
+get '/secret' do
+  "#{rand}"
+end
+
+get '/marco' do
+  '<p><span style="text-decoration: underline;"><em><strong><span style="color: #ff9900; text-decoration: underline;">Hello, And</span>rea!!!!</strong></em></span></p>'
 end
