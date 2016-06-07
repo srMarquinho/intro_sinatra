b3c9ce5cfdaab75a72b6f5c33bdbb6e27081fffc
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-06-07 15:01:04 +0100

    add picture with border

--- app.rb
+++ app.rb
@@ -9,6 +9,12 @@ get '/secret' do
   "#{rand}"
 end
 
-get '/marco' do
-  '<p><span style="text-decoration: underline;"><em><strong><span style="color: #ff9900; text-decoration: underline;">Hello, And</span>rea!!!!</strong></em></span></p>'
+get '/whatever' do
+  "
+  <body>
+    <div style='width:50%; display: block; margin-left: auto; margin-right: auto; border: 3px dashed blue; width:50%;'>
+        <img style='width:100%;' src='http://www.planwallpaper.com/static/images/cool-wallpaper-1.jpg'>
+    </div>
+  </body>
+  "
 end
