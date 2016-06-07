e022bf13ac979bd87104dca7f8cbdaa76d3d1e27
srMarquinho <sr.marquinho@gmail.com>
2016-06-07 15:24:45 +0100

    add random name

--- app.rb
+++ app.rb
@@ -9,12 +9,7 @@ get '/secret' do
   "#{rand}"
 end
 
-get '/whatever' do
-  "
-  <body>
-    <div style='width:50%; display: block; margin-left: auto; margin-right: auto; border: 3px dashed blue; width:50%;'>
-        <img style='width:100%;' src='http://www.planwallpaper.com/static/images/cool-wallpaper-1.jpg'>
-    </div>
-  </body>
-  "
+get '/cat' do
+  @rnd = rand
+  erb :index
 end
